6220c3c5b4461814e7650562d8aeef42414a28a9
unknown <velimir.tchatchevsky@gmail.com>
2017-08-21 10:50:28 +0300

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+$user = false;
+$host = 'http://ogicom.bg/scores/notes/func.php';
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+		//koda tuk!
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+			db.transaction(function (tx) {
+				tx.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name ='users'", [], function (tx, result) {
+					if (result.rows.length == 0) {
+						//show login screen
+						showLoginScreen();
+						//create table users, to be filled by the login function
+						db = window.sqlitePlugin.openDatabase({name: 'demo.db', location: 'default'});
+						db.transaction(function(tx) {
+							tx.executeSql('CREATE TABLE IF NOT EXISTS users (id, name, pass, avatar, position, email, verified)');
+						}, error, success);
+					} else {
+						//login(results.item(0).rows['name'], results.item(0).rows['password']);
+					}
+				});
+			}
+			
+		function showLoginScreen($err){
+			if($err !== undefined){
+				$("#loginScreen .err").html($err).show();
+			}
+			$("#loginScreen").fadeIn();
+		}
+		
+		$(document).on('click', '#loginScreen button', function(){
+			login($("#loginScreen .name").val(), $("#loginScreen .pass").val());
+		});
+		
+		function error(error){
+			alert('err'+error.message);
+		}
+		
+		function success(){
+			console.log('Populated database OK');
+		}
+		
+		function login(user, pass){
+			$.post($host, {'act':'login', 'user':user, 'pass':pass}, function(data){ 
+				data = JSON.parse(data);
+				if(data.logged == 'true'){
+					$user = user;;
+					$("#loginScreen").fadeOut();
+					db.transaction(function(tx) {
+						tx.executeSql('INSERT INTO users VALUES (?,?,?,?,?,?,?)', [1, user, pass, data.avatar, data.position, data.email, data.verified]);
+					}, error, success);
+					alert("logged");
+				}else{
+					var $err = data.err;
+					showLoginScreen($err);
+					//login screen show, error msg show
+				}
+			});
+		}//login		
+    }
+};
